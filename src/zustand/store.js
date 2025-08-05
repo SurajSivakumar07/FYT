502eae57a05b039c744d2097fb5fdc3680730b9d
Suraj S <surajsuri279@gmail.com>
2025-08-05 16:45:24 +0530

    added presisted storage for zustand for access

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -1,14 +1,31 @@
 // store/useUserStore.ts
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useUserStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null }),
-}));
+export const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: "user-storage", // key in localStorage
+      getStorage: () => localStorage, // or sessionStorage if you prefer
+    }
+  )
+);
 
-export const useAccessStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null }),
-}));
+export const useAccessStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: "access-storage", // key in localStorage
+      getStorage: () => localStorage, // you can also use sessionStorage
+    }
+  )
+);
